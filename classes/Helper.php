ed08adca9cd682c8238b22e5bf6a229e5b22ac1c
Emmett Framson <framsone@gmail.com>
2016-01-08 11:58:02 -0800

    Fixed method header which was apparently breaking

--- classes/Helper.php
+++ classes/Helper.php
@@ -2,7 +2,7 @@
 
 class Helper {
 
-	public function decamelize($input){
+	public static function decamelize($input){
 		preg_match_all('!([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)!', $input, $matches);
 		$ret = $matches[0];
 		foreach ($ret as &$match) {
